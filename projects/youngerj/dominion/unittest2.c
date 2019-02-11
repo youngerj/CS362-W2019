afcfccf9b5ed9d9d748f41bcfdc96bf8d816f7a7
Jarritt Youngers <youngerj@flip1.engr.oregonstate.edu>
2019-02-10 23:46:25 -0800

    Added unit tests

---
+++ projects/youngerj/dominion/unittest2.c
@@ -0,0 +1,63 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+
+#define NOISY_TEST 1
+
+int main(){
+
+	int k[10] = {adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall};
+    	struct gameState G;
+	
+	r = initializeGame(2, k, 1, &G);
+
+	printf("TESTING: buyCard()\n");
+
+#if (NOISY_TEST == 1)
+	printf("Testing buyCard with no money: \n");
+#endif
+  G.numBuys = 1;
+  G.coins = 0;
+
+	if(buyCard(1, &G) == -1){
+		printf("test passed! \n");
+   }
+	else{
+		printf("test failed! \n");
+  }
+  
+#if (NOISY_TEST == 1)
+	printf("Testing buyCard with no buys left: \n");
+#endif
+  G.numBuys = 0;
+  G.coins = 6;
+
+	if(buyCard(2, &G) == -1){
+		printf("test passed! \n");
+   }
+	else{
+		printf("test failed! \n");
+  }
+    
+#if (NOISY_TEST == 1)
+	printf("Testing buyCard on good card: \n");
+#endif	
+
+  G.numBuys = 1;
+  G.coins = 10;
+
+  if(buyCard(2, &G) == 0){
+		printf("test passed! \n");	
+	}
+	else{
+		printf("test failed! \n");
+	}
+
+	printf("Testing complete! \n");
+ 
+  return 0;
+
+}
---
+++ projects/youngerj/dominion/unittest3.c
@@ -0,0 +1,55 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+
+#define NOISY_TEST 1
+
+int main(){
+
+	int k[10] = {adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall};
+    	struct gameState G;
+	
+	r = initializeGame(2, k, 1, &G);
+
+	printf("TESTING: supplyCount() with 2 cards (adventurer and smithy)\n");
+
+#if (NOISY_TEST == 1)
+	printf("Testing supplyCount of adventurer: \n");
+#endif
+  G.supplyCount[0] = 5;
+	if(supplyCount(0,&G) == 5){
+		printf("test passed! \n");
+	}
+	else{
+		printf("test failed! \n");
+	}
+
+#if (NOISY_TEST == 1)
+	printf("Testing supplyCount of smithy: \n");
+#endif
+  G.supplyCount[6] = 5;
+	if(supplyCount(6,&G) == 5){
+		printf("test passed! \n");
+	}
+	else{
+		printf("test failed! \n");
+	}
+
+#if (NOISY_TEST == 1)
+	printf("Testing supplyCount of non-exisistant card: \n");
+#endif	
+	if(supplyCount(-5,&G) == -1){
+		printf("test passed! \n");
+	}
+	else{
+		printf("test failed! \n");
+	}
+
+	printf("Testing complete! \n");
+
+
+	return 0;
+}
